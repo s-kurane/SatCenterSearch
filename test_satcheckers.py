4edfdc6546220857980aa0680c8a999eb9a9455f
s-kurane <53353649+s-kurane@users.noreply.github.com>
2024-02-08 22:44:36 -0800

    Update test_satcheckers.py

--- test_satcheckers.py
+++ test_satcheckers.py
@@ -16,9 +16,9 @@ from selenium.webdriver.support.ui import Select
 
 import smtplib
 
-email = "youremail@gmail.com"
-location = "00000" #your zip code
-distance = "10 miles" #whatever your want (has to be on the website as an option)
+email = "skurane@gmail.com"
+location = "94555" #your zip code
+distance = "50 miles" #whatever your want (has to be on the website as an option)
 class TestSatchecker():
     def setup_method(self):
         service = Service('chromedriver.exe')
